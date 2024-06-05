e8a21fe3daaad2da5b3065eb99561ded59eeb783
gabjc <gabtez23@gmail.com>
2024-06-05 16:55:00 -0400

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,50 +21,49 @@ public class Main {
       // Since the tester restarts your program quite often, setting SO_REUSEADDR
       // ensures that we don't run into 'Address already in use' errors
       serverSocket.setReuseAddress(true);
-      clientSocket = serverSocket.accept(); // Wait for connection from client.
+      while (true) {
+        clientSocket = serverSocket.accept(); // Wait for connection from client.
+                
+        // Gets the input stream
+        InputStream input = clientSocket.getInputStream();
+        // Reads text from character input stream
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input)); 
+        String line = reader.readLine();
 
-      // Server will respond with a 200 OK response Stage 2
-      // clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-      // System.out.println("accepted new connection");  
-      
-      // Gets the input stream
-      InputStream input = clientSocket.getInputStream();
-      // Reads text from character input stream
-      BufferedReader reader = new BufferedReader(new InputStreamReader(input)); 
-      String line = reader.readLine();
+        //Testing line output
+        System.out.println(line);
 
-      //Testing line output
-      System.out.println(line);
-
-      // Splitting the line based on spaces
-      String[] HttpRequest = line.split(" ", 0);
+        // Splitting the line based on spaces
+        String[] HttpRequest = line.split(" ", 0);
 
 
-      // Testing HttpRequest outputs
-      for (String arr : HttpRequest) {
-        System.out.println(arr);
-      }
+        // Testing HttpRequest outputs
+        for (String arr : HttpRequest) {
+          System.out.println(arr);
+        }
 
-      // Initialized for the write function
-      OutputStream output = clientSocket.getOutputStream();
-      
-      if (HttpRequest[1].equals("/")) {
-        output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-      } else if (HttpRequest[1].startsWith("/echo/")) {
-        // Get the rest of the string after "/echo/"
-        String message = HttpRequest[1].substring(6);
-        String str = String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", message.length(), message);
-        output.write(str.getBytes());
-      } else if (HttpRequest[1].startsWith("/user-agent")) {
-        reader.readLine();
-        String userAgent = reader.readLine();
-        String body = userAgent.substring(12);
-        String str = String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", body.length(), body);
-        output.write(str.getBytes());
-      } else {
-        output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
-      }
-
+        // Initialized for the write function
+        OutputStream output = clientSocket.getOutputStream();
+        
+        if (HttpRequest[1].equals("/")) {
+          output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+        } else if (HttpRequest[1].startsWith("/echo/")) {
+          // Get the rest of the string after "/echo/"
+          String message = HttpRequest[1].substring(6);
+          String str = String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", message.length(), message);
+          output.write(str.getBytes());
+        } else if (HttpRequest[1].startsWith("/user-agent")) {
+          // Skip the Host header
+          reader.readLine();
+          // Get the User-Agent header
+          String userAgent = reader.readLine();
+          String body = userAgent.substring(12);
+          String str = String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", body.length(), body);
+          output.write(str.getBytes());
+        } else {
+          output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+        }
+    }
 
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
