87ebe10c3f6a9b2ab3c25e6e587d878e91eee425
gabjc <gabtez23@gmail.com>
2024-06-05 15:52:46 -0400

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,9 +21,21 @@ public class Main {
       // ensures that we don't run into 'Address already in use' errors
       serverSocket.setReuseAddress(true);
       clientSocket = serverSocket.accept(); // Wait for connection from client.
-      clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+
+      // Server will respond with a 200 OK response Stage 2
+      // clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+      // System.out.println("accepted new connection");  
       
-      System.out.println("accepted new connection");  
+      // Gets the input stream
+      InputStream input = clientSocket.getInputStream();
+      // Reads text from character input stream
+      BufferedReader reader = new BufferedReader(new InputStreamReader(input)); 
+      String line = reader.readLine();
+      //Testing
+      System.out.println(line);
+
+
+
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }    
