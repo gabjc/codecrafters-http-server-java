9d4860c5c5307f2e581d96311555721ddcc181f8
gabjc <gabtez23@gmail.com>
2024-06-06 19:39:04 -0400

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -20,6 +21,8 @@ public class Main {
     if (args.length == 2) { 
       directory = args[1];
     }
+    
+    System.out.println("Directory: " + directory);
     
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
@@ -57,12 +60,16 @@ public class Main {
         
         if (HttpRequest[1].equals("/")) {
           output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-        } else if (HttpRequest[1].startsWith("/echo/")) {
+        } 
+        // IMPLEMENTING ENDPOINT AND RESPONDING WITH STRING
+        else if (HttpRequest[1].startsWith("/echo/")) {
           // Get the rest of the string after "/echo/"
           String message = HttpRequest[1].substring(6);
           String str = String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", message.length(), message);
           output.write(str.getBytes());
-        } else if (HttpRequest[1].startsWith("/user-agent")) {
+        } 
+        // READING THE USER-AGENT HEADER
+        else if (HttpRequest[1].startsWith("/user-agent")) {
           // Skip the Host header
           reader.readLine();
           // Get the User-Agent header
@@ -70,7 +77,9 @@ public class Main {
           String body = userAgent.substring(12);
           String str = String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", body.length(), body);
           output.write(str.getBytes());
-        } else if (HttpRequest[1].startsWith("/files")) {
+        } 
+        // RETURNING A FILE
+        else if (HttpRequest[1].startsWith("/files")) {
           // Get the file and read from the file
           String fileName = HttpRequest[1].substring(7);
           File file = new File(directory, fileName);
@@ -83,7 +92,23 @@ public class Main {
           } else {
               output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
           }
-        } else {
+        } 
+        // POST A FILE
+        else if (HttpRequest[0].equals("POST") && HttpRequest[1].startsWith("/files")) {
+          
+          String fileName = HttpRequest[1].substring(7);
+          FileWriter file = new FileWriter(fileName);
+
+          String body = reader.readLine();
+          System.out.println("Body: " + body);
+
+          file.write(body);
+          file.close();
+
+          String str = String.format("HTTP/1.1 201 Created");
+          output.write(str.getBytes());
+        }
+        else {
           output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
         }
     }
